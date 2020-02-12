2168208d0e837951d2c844865be4687883ada9fc
Daniel Urbina <danielur1999@gmail.com>
2020-02-12 21:14:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -82,14 +82,13 @@ def gross_per_studio(collection)
     if gross_total[current_studio_name]
       gross_total[current_studio_name] += current_studio_gross
     else
-      
       gross_total[current_studio_name] = current_studio_gross
     end
     
     i += 1
   end
   
-  gross_total
+  p gross_total
 end
 
 def movies_with_directors_set(source)
